dc081c721b09ced559c529c2a1cde84fe58508f1
Romario <moises.rrr199@gmail.com>
2026-04-17 12:35:07 -0500

    Fix JWT expiresIn typing

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import type { StringValue } from 'ms';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -13,16 +14,21 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.getOrThrow<string>('JWT_SECRET'),
-        signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRES_IN') ?? '30d',
-        },
-      }),
+      useFactory: (configService: ConfigService) => {
+        const expiresIn = (
+          configService.get<string>('JWT_EXPIRES_IN') ?? '30d'
+        ) as StringValue;
+
+        return {
+          secret: configService.getOrThrow<string>('JWT_SECRET'),
+          signOptions: { expiresIn },
+        };
+      },
     }),
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
+
 
